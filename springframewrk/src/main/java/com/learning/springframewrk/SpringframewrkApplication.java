5a642300ce4e98ce7eab92b8d92ea57b58d38cfb
Sanjay.murpani <smurpani@deloitte.com>
2020-08-03 19:33:49 +0530

    First

---
+++ springframewrk/src/main/java/com/learning/springframewrk/SpringframewrkApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springframewrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframewrkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringframewrkApplication.class, args);
+	}
+
+}
---
+++ springframewrk/src/test/java/com/learning/springframewrk/SpringframewrkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springframewrk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframewrkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
